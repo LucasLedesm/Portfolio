cdfbc9c40b9c05bc109d8b2a1d9169cc8843b91a
Lucas Ledesma <lucasledesma27@gmail.com>
2022-09-24 18:07:08 -0300

    boostrapSass

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbaar from './components/Navbar/Navbaar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbaar/>
+    </div>
+  );
+}
+
+export default App;
