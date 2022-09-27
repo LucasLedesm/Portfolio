df4c6c78e8fe1ac1fe6db3b292294339dd755f9a
Lucas Ledesma <lucasledesma27@gmail.com>
2022-09-27 15:10:33 -0300

    correciones3

--- portfolio/src/pages/About/About.jsx
+++ portfolio/src/pages/About/About.jsx
@@ -10,6 +10,11 @@ import { DiHtml5 } from "react-icons/di";
 import { DiNodejsSmall } from "react-icons/di";
 import { DiReact } from "react-icons/di";
 import { DiSass } from "react-icons/di";
+import { SiFigma } from "react-icons/si";
+import { SiVisualstudio } from "react-icons/si";
+import { SiPostman } from "react-icons/si";
+import { SiGoogle } from "react-icons/si";
+
 
 import Container from 'react-bootstrap/esm/Container';
 import Col from 'react-bootstrap/Col';
@@ -19,37 +24,46 @@ import Row from 'react-bootstrap/Row';
 
 const About = () => {
   return (
-    <Container classNameName='AboutPage'>
+    <div id='AboutPage'>
+      <Container>
       <Row>
         <Col>
-        <h1 className='Title'>Professional Skillset</h1>
+          <h1 className='Title'><span className='colorText'>Professional</span> Skillset</h1>
         </Col>
       </Row>
       <Row>
-        <Col xs={4} md={2} className="bn5 Card-Tech"><DiCss3 /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiBootstrap /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiGithubAlt /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiGit /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiSass /></Col>
+        <Col xs={6} md={3} className="bn5 Card-Tech"><DiCss3 /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><DiBootstrap /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><DiGithubAlt /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><DiGit /></Col>
+
         <div className="w-100"></div>
-        <Col xs={4} md={2} className="Card-Tech"><DiFirebase /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiJavascript1 /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiHtml5 /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiNodejsSmall /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiReact /></Col>
+
+        <Col xs={6} md={3} className="Card-Tech"><DiSass /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><DiFirebase /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><DiJavascript1 /></Col>
+
+        <div className="w-100"></div>
+
+        
+        <Col xs={6} md={3} className="Card-Tech"><DiHtml5 /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><DiNodejsSmall /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><DiReact /></Col>
       </Row>
       <Row>
         <Col>
-        <h1 className='Title'>Tools I use</h1>
+          <h1 className='Title'><span className='colorText'>Tools</span> I use</h1>
         </Col>
       </Row>
       <Row>
-        <Col xs={4} md={2} className="bn5 Card-Tech"><DiCss3 /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiBootstrap /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiGithubAlt /></Col>
-        <Col xs={4} md={2} className="Card-Tech"><DiGit /></Col>
+        <Col xs={6} md={3} className="bn5 Card-Tech"><SiFigma /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><SiVisualstudio /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><SiPostman /></Col>
+        <Col xs={6} md={3} className="Card-Tech"><SiGoogle /></Col>
+        
       </Row>
     </Container>
+    </div>
   )
 }
 
